445446fe88dafc7478b56a06f65bba3c242ae1b1
Progress Aienobe <paienobe@gmail.com>
2021-12-22 19:26:03 +0100

    Adding a module with data to be displayed

---
+++ data.js
@@ -0,0 +1,35 @@
+const reviewContent = [
+    `<div class="review-block">
+                          <img src="./images/avatar-anisha.png" alt="avatar" class="avatar">
+                          <div class="review-text">
+                              <h4>Anisha Li</h4>
+                              <p>“Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated.”</p>
+                          </div>
+                          </div>`,
+  
+    `<div class="review-block">
+                          <img src="./images/avatar-ali.png" alt="avatar" class="avatar">
+                          <div class="review-text">
+                              <h4>Ali Bravo</h4>
+                              <p>“We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused.”</p>
+                          </div>
+                          </div>`,
+  
+    `<div class="review-block">
+                      <img src="./images/avatar-richard.png" alt="avatar" class="avatar">
+                      <div class="review-text">
+                          <h4>Richard Watts</h4>
+                          <p>“Manage allows us to provide structure and process. It keeps us organized and focused. I can’t stop recommending them to everyone I talk to!”</p>
+                      </div>
+                      </div>`,
+  
+    `<div class="review-block">
+                      <img src="./images/avatar-shanai.png" alt="avatar" class="avatar">
+                      <div class="review-text">
+                          <h4>Shanai Gough</h4>
+                          <p>“Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive.”</p>
+                      </div>
+                      </div>`,
+  ]
+
+export default reviewContent 
